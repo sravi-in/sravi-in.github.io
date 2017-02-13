bd913f047e8d20e7e700e743f5b4794a3e85a463
Ravi Surulikamu <sravi.in@gmail.com>
2017-02-12 23:46:51 -0800

    OPOS Recipes

---
+++ js/recipes.js
@@ -0,0 +1,158 @@
+var recipes = [
+{name:  "Standardisation "
+, recipe: "In a 2L pressure cooker, add 1/4C water (60ml). Cook on 'high' so as to get a long & loud whistle in 1 to 2 minutes. Reduce/ increase heat to achieve this timing. 'High' is around 1000W on induction. On gas, the flame just about covers the base. Electric stoves need preheating."
+, link: "https://www.youtube.com/watch?v=5oGCV4xBh3c"},
+{name:  "Dulce De Leche "
+, recipe: "In a 2L cooker, add 1tin of sealed, sweet condensed milk (400g), after removing paper labels. Add 2.5C water. Cook on high for 30min (ignore whistles). Let cool."
+, link: "https://www.youtube.com/watch?v=DK8-EvChiX0"},
+{name:  "Kesari "
+, recipe: "In a 2L cooker, add 2C water, 3tbsp ghee, 1/8tsp cardamom powder, 1tsp saffron, 3/4C sugar. Cook for 1 whistle/ 6mins on high. Switch off. Release pressure. Mix in 1/2C double roasted fine rava. Close & rest 20 mins. Mix well."
+, link: "https://www.youtube.com/watch?v=eLH8YMPYu1M"},
+{name:  "Bisi Bele Bhath "
+, recipe: "In a 2L cooker, add 3C water, 1/2C each (rice, tuvar dal), 3tbsp grated & roasted coconut, 1tbsp each (tamarind pulp, ghee),1tsp each (sambar powder, salt, jaggery), 1/2tsp each (chilli powder, garam masala), 1/4tsp each (turmeric powder, asafoetida). Cook on high for 8 whistles/ 10 mins. Let pressure settle."
+, link: "https://www.youtube.com/watch?v=nABJlZtrHno"},
+{name:  "Rava Upma "
+, recipe: "In a 2L cooker, add 2C water, 1/2C chopped onions, 1tbsp coconut oil, 1tsp chopped ginger, 1/2tsp salt, 6 curry leaves, 2 chopped green chillies . Close & cook for 1 whistle/ 6mins on high. Switch off. Release pressure. Mix in 1/2C double roasted fine rava. Keep closed for 20 mins without heating. Mix & serve."
+, link: "https://www.youtube.com/watch?v=4XimOVq3OX0"},
+{name:  "Sherbet "
+, recipe: "In a 2L cooker, add 1C sugar, 1C water, 10g cinnamon sticks (1/4C ), 6 cloves, 6 crushed cardamoms. Cook on high for 1 whistle/ 4 mins. Let cool & bottle. Mix with chilled water/ lemon juice/ fruit juice. Use in place of sugar in tea/ coffee."
+, link: "https://www.youtube.com/watch?v=y6R1lstD_m0"},
+{name:  "Minestrone soup"
+, recipe: "In a 2L cooker, add 2C water, 1C mixed vegetables (carrot, beans, capsicum, fresh peas), 1/2C tomato paste, 1/4C macaroni, 1tbsp butter, 1tsp each (pepper, salt), 1/2tsp mixed herbs (Basil, oregano, rosemary), 3 crushed garlic cloves. Cook on high for 2 whistles/ 7 mins."
+, link: "https://www.youtube.com/watch?v=kl5N0PBd5xA"},
+{name:  "Room Freshner Rasam "
+, recipe: "In a 2L cooker, add 2C water, 1/4C each chopped (cilantro roots, drumsticks), 1tsp ginger, 1/2tsp salt, 1 dry red chilli, 1/4tsp asafetida, 1 chopped tomato, 4 crushed shallots, 6 curry leaves, 1 slit green chill, 1 crushed garlic clove. Cook on high for 2 whistles/ 6 mins. Let pressure settle. Filter out solids. "
+, link: "https://www.youtube.com/watch?v=XRKs6yIudQk"},
+{name:  "Boiled Eggs "
+, recipe: "In a 2L cooker, add 15 eggs, 1.5C water. Cook on high for 1 whistle/ 9 mins. Switch off. Soft-boiled eggs: Release pressure & serve. Medium boiled eggs: Release pressure. Wait for 5 mins & serve. Hard boiled eggs: Let pressure settle. Peel & serve. "
+, link: "https://www.youtube.com/watch?v=JJ40ETJKEgY"},
+{name:  "Cooking Dal "
+, recipe: "In a 2L cooker, add 1C tuvar dal, 2C water, 1tsp ghee. Close & cook on high for 8 whistles/ 7 minutes. Refrigerate for weeks or freeze for months."
+, link: "https://www.youtube.com/watch?v=ISXjLyE0rfU"},
+{name:  "Cooking Rice "
+, recipe: "Basmathi : In a 2L cooker, add 1C basmathi rice, 1.5C water. Close & cook on high for 2 whistles/ 5minutes. Raw rice: 1C rice, 2.5C water, 2 whistles/ 7 minutes. Parboiled rice: 1C rice, 3C water, 3 whistles/ 8 minutes."
+, link: "https://www.youtube.com/watch?v=ISXjLyE0rfU"},
+{name:  "Tambli"
+, recipe: "Blend 1/2C yogurt, 1/4C coconut, 1/2tsp cumin. 1/4tsp salt, 1 green chilli. Serve with rice."
+, link: "https://www.youtube.com/watch?v=8_TxKk7DGi8"},
+{name:  "Sundal "
+, recipe: "In a 2L cooker, add 1C channa dal (soaked 2 hrs & drained), 1tsp ginger, 6 curry leaves, 1/2tsp salt, 1/8tsp turmeric & 1/2C water. Cook on high for 2whistles / 4 minutes. Let settle. Serve as a snack."
+, link: "https://www.youtube.com/watch?v=jQRR8bp3qlw"},
+{name:  "Attalysis "
+, recipe: "Step1: Take 250g whole-wheat flour & 250ml water. Add 1/2tsp salt & knead as usual. Measure water left over after kneading (say 75 ml). Step 2: Now, take 175 ml water ( 250ml - 75ml). Add1/2tsp salt & 250g flour. Mix till all water is absorbed (do not knead). Cover & rest 30 mins. Knead briefly & use."
+, link: "https://www.youtube.com/watch?v=ppObIbz8mJI"},
+{name:  "Paneer "
+, recipe: "In a 2L cooker, add 500ml full cream milk, 1/4C sour yogurt. Cook for 2 whistles/ 5 minutes on high. Let pressure settle. Strain & mould milk solids."
+, link: "https://www.youtube.com/watch?v=R34Pps38u1M"},
+{name:  "Tamarind paste "
+, recipe: "In a 2L cooker, add 250g (2C) tamarind ( seeds & fiber removed). Add 2C water ( Just enough to cover tamarind). Cook on high for 4 whistles. Mash & bottle."
+, link: "https://www.youtube.com/watch?v=5TBv6H7YOFk"},
+{name:  "Bhapa Doi "
+, recipe: "Smear a small vessel with 2tsp ghee. Add 1C sweet condensed milk, 1C thick yogurt. Mix well. In a 2L cooker, add 1.5C water. Place the small vessel inside cooker. Cook on medium heat for 15 mins (5 whistles). Let settle. Remove & unmould."
+, link: "https://www.youtube.com/watch?v=RUHt6qrBcA4"},
+{name:  "Sago Kheer "
+, recipe: "In a 2L cooker, add 1/2C sago ( soaked till soft & drained), 1C water, saffron, 1tsp ghee, 1/8tsp cardamom powder. Cook for 2 whistles/ 4 minutes on high. Mix in 1/2C sugar. Close & rest 10 mins."
+, link: "https://www.youtube.com/watch?v=yRYrChrIZ8g"},
+{name:  "Dum Tea "
+, recipe: "In a 2L cooker, add 1C milk, 1/4tsp dry ginger powder, 1in bit cardamom, 1 clove, 2tsp tea leaves. Cook on high for 1 whistle. Open after 5 minutes. Filter, mix in sugar & serve. Can use just 1/2C water instead of 1C milk & mix in hot milk after opening."
+, link: "https://www.youtube.com/watch?v=RMohheVMhlo"},
+{name:  "Mac 'n' Cheese "
+, recipe: "In a 2L pressure cooker, add 1tbsp butter, 1C macaroni (85g) & enough water to cover pasta completely (1C). Stir well. Cook on high for 2 whistles (3 minutes). Release pressure. Mix in 1/2C grated cheddar cheese. Soak/ cook longer if pasta is undercooked."
+, link: "https://www.youtube.com/watch?v=KUw6thZvJhw"},
+{name:  "Flashcooking "
+, recipe: "In a 2L pressure cooker, add 1tsp oil, 3tbsp water, 2C chopped vegetables (250g), 1/4tsp each (salt, chilli powder). Cook on high for 2 whistles/ 4minutes. Release pressure. "
+, link: "https://www.youtube.com/watch?v=yPDqNweqamo"},
+{name:  "Kaanda batata poha "
+, recipe: "In a 2L cooker, add 3tbsp water, 1tsp each (oil, ginger, green chilli), 1C each chopped (onion, potato), 1tsp salt. Cook on high for 2 whistles/ 5min. Release pressure. Mix in 2C thin rice flakes, 1tsp cilantro, 1/4C roasted peanuts, 2tsp lemon juice. Keep closed for 10 mins."
+, link: "https://www.youtube.com/watch?v=QSJJNYmR_v0"},
+{name:  "Sambar "
+, recipe: "In a 2L cooker, add 3tbsp water, 1tsp oil, 4 curry leaves, 2C chopped white pumpkin, 1C cooked tuvar dal mixed with 1tsp each (tamarind paste, sambar powder, salt), 1/8 tsp each (turmeric powder, asafoetida). Cook on high for 2whistles (6 min). Mix all."
+, link: "https://www.youtube.com/watch?v=uB15G-f5FP0"},
+{name:  "Kootu "
+, recipe: "Blend 1/2C coconut, 1tsp cumin, 2 green chillies, 1tsp poppy seeds, 1/4C water. In a 2L cooker, add 1/4C water, 1C chopped white pumpkin, 1/2C (1 hour) soaked channa dal , 1/2 tsp salt, 1/8 tsp turmeric powder, coconut paste. Cook for 1 whistle/ 6 min on high. Release pressure. Mix all."
+, link: "https://www.youtube.com/watch?v=8B9VGj3VNUQ"},
+{name:  "Kuruma "
+, recipe: "Mix 1/2C coconut paste, 2tbsp cashew powder, 1tsp each (salt, ginger-garlic paste, garam masala), 1tbsp chilli paste. In a 2L cooker, add 1tbsp ghee, 3tbsp water, 2C chopped mixed vegetables (carrot, beans, potato), coconut paste. Cook on high for 4 whistles (7 mins). Mix in 1/2C coconut milk. "
+, link: "https://www.youtube.com/watch?v=Cw8eVP8mmG4"},
+{name:  "Aviyal "
+, recipe: "Blend 1/2C coconut, 2tbsp water, 1tsp cumin, 1chilli, 2tsp coconut oil, 1tsp salt. In a 2L cooker, add 3tbsp water, 1/2C each chopped vegetables (pumpkin, carrot, broad beans, squash, raw banana), coconut paste. Cook on high for 2 whistles/ 6 minutes. Release pressure. Mix in 1C yogurt, 6 crushed curry leaves. "
+, link: "https://www.youtube.com/watch?v=aWv_UcqPFXc"},
+{name:  "Enna Kathirikka Kozhambu "
+, recipe: "Mix 3tbsp each (tamarind paste, sambar powder), 2tsp salt,1tsp (jaggery, salt, dhania powder), 1/4 tsp (turmeric , fenugreek powders). Slit baby eggplants (250g) & smear paste inside. In a 2L cooker, add 3tbsp each (sesame oil, water), 1C onions, 5 small whole tomatoes, eggplants. Cook on high for 4 whistles/ 6minutes . Mash/ blend gravy."
+, link: "https://www.youtube.com/watch?v=MGt-MYaOiQE"},
+{name:  "Gutti Vankaya Kura "
+, recipe: "Slit 250g baby eggplants into 4 so that they remain joint at the base. Stuff with a spice mix (puliyogare powder/ spiced lentil powder etc). In a 2l cooker, add 3tsp oil, 3tbsp water, eggplants. Cook for 2 whistles/ 6 mins on high."
+, link: "https://www.youtube.com/watch?v=nNkevnQF8NM"},
+{name:  "Kaara Chutney "
+, recipe: "In a 2L pressure cooker, add 1/4C sesame oil, 3tbsp water, 1C chopped onion, 1/4C chopped shallots, 3 deseeded tomatoes , 5 dry red chillies, 1tsp each (tamarind pulp, salt, jaggery). Cook on high for 5mins/ 4 whistles. Blend all."
+, link: "https://www.youtube.com/watch?v=UhGn2kfgr7k"},
+{name:  "Usili "
+, recipe: "Blend 1/2C channa dal (soaked 2 hrs & drained), 2 dry red chillies, 1/2tsp salt,1/8 tsp asafoetida. Shape into patties. In a 2L cooker add 3tbsp water, 1tbsp oil, 1C chopped beans, 1/4 tsp salt, channa dal patties. Cook on high for 2 whistles. Release pressure. Crumble patties. Mix all."
+, link: "https://www.youtube.com/watch?v=ZI__RikhCto"},
+{name:  "Pav Bhaji "
+, recipe: "In a 2l cooker, add 3tbsp each (butter, water), 1/2c each (onion, tomato, potato, beans, carrot, capsicum ) , 1tsp ginger garlic paste, 1/2tsp each (salt, chilli, cumin, dhania, garam masala, kasuri methi), 1/4tsp turmeric powder. Cook for 2W/ 6min on high. Let settle. Mash/ blend all."
+, link: "https://www.youtube.com/watch?v=okeebTyiWWU"},
+{name:  "Paneer butter masala "
+, recipe: "In a 2l cooker add 3tbsp each (butter, water), 1C onions, 4 whole small tomatoes, 1tsp (Kashmiri chilli powder, salt, ginger- garlic paste, salt, kasoori methi), 1/2tsp (coriander, cumin, garam masala). Cook on high for 3W/ 5mins. Mash/ blend all. Mix in 1/2C chopped paneer."
+, link: "https://www.youtube.com/watch?v=xnfEgkn9afc"},
+{name:  "Methi Muttar Malai "
+, recipe: "In a 2L cooker, add 3tbsp water, 1tbsp butter, 1.5C fresh peas, 1/2C fresh fenugreek leaves, 1/2tsp each (dhania, cumin, gram masala powders), 1/2tsp each (chill paste, salt, ginger- garlic paste). Cook for 5 minutes/ 1 whistle on high. Switch off. Release pressure. Mix in 1/4C thick cream."
+, link: "https://www.youtube.com/watch?v=JGZJrC616Kc"},
+{name:  "Paalak Paneer "
+, recipe: "In a 2L cooker, add 1tsp water, 1tsp oil, 1/4c each (onion, tomato), 1tsp ginger garlic paste, 3C spinach, 1 chopped chilli, 1/2tsp kasoori methi, 1/2tsp each (salt, cumin, dhania , garam masala powders). Cook on high for 2 whistles/ 5 minutes. Release pressure. Mash/ blend spinach. Mix in 1C paneer cubes."
+, link: "https://www.youtube.com/watch?v=Awcl5Fv8Q6I"},
+{name:  "Kadai Paneer "
+, recipe: "Mix 125g paneer (1C), 1tsp each (salt, chili powder, kasoori methi), 1/2tsp each (garam masala, cumin, coriander powders). In a 2L pressure cooker, add 1tbsp water, 1tsp oil, 1C each chopped (onion, capsicum, deseeded tomato), paneer. Cook on high for 2 whistles/ 4 minutes. Release pressure. Mix all."
+, link: "https://www.youtube.com/watch?v=KV_uxWNZtMw"},
+{name:  "Malai Kofta "
+, recipe: "Blend 1C paneer with 1/2tsp each (salt, chilli powder). Make tight, smooth, marble sized balls. in a 2L cooker, add 2tbsp water, 1tbsp ghee, 1C onions, 2 slit chillies, 1/2tsp each ( ginger garlic paste, coriander, cumin, garam masala powders), 1/4C almond paste, paneer balls. Cook on high for 2 whistles/ 5 minutes. Blend all but paneer with 1/2C milk/ cream."
+, link: "https://www.youtube.com/watch?v=sDZrzZD-P2o"},
+{name:  "Tava Pulao "
+, recipe: "In a 2L cooker, add 1tbsp oil, 3tbsp water, 2C mixed vegetables, 1/4C roasted peanuts, 2tsp mung dal, 1/2tsp chilli powder, 1/4tsp each (asafetida, turmeric powder), 1tsp salt. Cook on high for 1 whistle/ 6 mins. Release pressure. Mix with 4C cooked & cooled rice."
+, link: "https://www.youtube.com/watch?v=o2UEA3xlwD8"},
+{name:  "Lunch box recipes "
+, recipe: "In a 2L pressure cooker, add 1tsp oil, 3tbsp water, 1C each chopped (onions, deseeded tomato, potato), 1/4C fresh green peas, 1 chopped chilli, 1/2tsp each (chilli, dhania, cumin, garam masala)powders, 1/2tsp salt. Cook on high for 2 whistles/ 5 minutes. Release pressure. Mix in 3C starch (rice/ pasta/ cooked millets/chopped flatbreads/ rice flakes,...)"
+, link: "https://www.youtube.com/watch?v=6p4cxGfp8z8"},
+{name:  "Caramelised onions "
+, recipe: "In a 2L pressure cooker, add 1tbsp water, 1tbsp oil, 3C (400g)chopped onions, 1tsp sugar, 1/2tsp salt. Cook on high for 6 minutes/ 8 whistles. Release pressure. Mix all. Stir fry briefly if needed. Bottle and use in place of onions."
+, link: "https://www.youtube.com/watch?v=fFblY3aVd7E"},
+{name:  "Dum Biriyani "
+, recipe: "Mix 1/2C each ch (carrot, beans potato), 1 slit chilli, 1/2C soaked soya chunks/ paneer, 3/4C yogurt, 1/4C fried onions, 1/2tsp each ( salt, chilli powder, garam masala). In a 2L cooker, add 3tbsp water, 1tbsp ghee, & half the vegetables. Add 1C soaked basmathi, 1/4C mint. Cover rice with remaining vegetables. Sprinkle 1/2C water. Cook on high for 2 whistles. Let pressure settle. Fluff up."
+, link: "https://www.youtube.com/watch?v=Mm8nDFiwMdc"},
+{name:  "Dum Noodles "
+, recipe: "In a 2L pressure cooker, add 3tbsp water, 1tsp oil, 1/2C each chopped (carrot, beans), 1 pack (85g) crushed, washed & drained wheat noodles (not instant noodles). Add 1tsp each (soy sauce, chili sauce, vinegar), 1/2tsp salt, 1/2C water. Cook on high for 2 whistles (5 minutes). Release pressure."
+, link: "https://www.youtube.com/watch?v=tqEF7R7AO9k"},
+{name:  "Ginger Garlic Paste "
+, recipe: "In a 2L pressure cooker, add 3tbsp oil, 3tbsp water, 1C each (crushed garlic cloves, skinned & chopped ginger). Cook for 3min/ 5 whistles on high. Release pressure & blend to a paste. Refrigerate."
+, link: "https://www.youtube.com/watch?v=VOObcdA89Cg"},
+{name:  "Pasta Arrabiatta "
+, recipe: "In a 2L cooker, add 3tbsp water, 1tsp olive oil, 1.5C chopped tomato, 3 chopped garlic cloves, 1/2tsp each (chilli powder, salt), 1C macroni. Cover with 1.5C chopped tomato. Cook on high for 3 whistles/ 6 mins. Release pressure. Fluff up & keep closed for 5 mins.  Undercooked? Use soaked pasta."
+, link: "https://www.youtube.com/watch?v=mnjXYhqerWk"},
+{name:  "Tomato Soup "
+, recipe: "In a 2L cooker, add 3tbsp water, 1tbsp butter, 1garlic clove, 1/2C onion, 5 whole medium tomatoes (250g) . Cook on high for 5 to 10 mins. Release pressure. Blend all with 1/2tsp each (salt, sugar, pepper)."
+, link: "https://www.youtube.com/watch?v=rlS1DWrv3wY"},
+{name:  "Carrot halwa "
+, recipe: "In a 2l cooker, add 3tsp water, 1C grated carrot, 1/2C sugar, 3tsp ghee. Cook on high for 8 minutes (or till you smell burning) Release pressure. Mix all."
+, link: "https://www.youtube.com/watch?v=7exWHt5g5Yo"},
+{name:  "Thakkali Thokku "
+, recipe: "In a 2L cooker, add 2tbsp sesame oil, 3tbsp water, 500g chopped deseeded tomato, 2tbsp chilli powder, 1tbsp each (salt, jaggery), 1/2tsp each (turmeric, asafoetida). Do not mix. Cook on high for 10 minutes or till you smell burning. Ignore whistles."
+, link: "https://www.youtube.com/watch?v=-kXoBR4RJ6g"},
+{name:  "Ghee "
+, recipe: "In a 2L cooker, add 250g butter. Cook on high. Time the gap between whistles. Switch off when the gap increases/ when you start getting a burnt smell. Takes around 7 mins/ 15 whistles depending on the water content in butter."
+, link: "https://www.youtube.com/watch?v=jwqA1xDyZIw"},
+{name:  "Mysorepak "
+, recipe: "In a 2L pressure cooker, add 1C sugar (200g), 1/4C water (60 ml). Cook on high for 4 or 6 whistles ( hard or soft mysorepak). Switch off. Release pressure. Open & mix in 1C (100g) well roasted gram flour, 1/8C ghee. Pour into a greased mould and let settle. Slice."
+, link: "https://www.youtube.com/watch?v=IcUWcS6XN5M"},
+{name:  "Badam Halwa "
+, recipe: "Soak 1C almonds in water for atleast 3 hours. Drain & blend to a paste without water. In a 2L cooker, add 1C (200g) sugar, 1/4C (60ml) water. Cook on high for 8 whistles. Release pressure. Mix in almond paste, 1tsp ghee."
+, link: "https://www.youtube.com/watch?v=-mGdHQR5Fmw"},
+{name:  "Bottled tadka "
+, recipe: "In a 2l cooker, add 1C sesame oil, 1/4C mustard seeds, 1/2C dry red chillies, 1/2C curry leaves, 1tbsp asafoetida. Cook on high for 5 mins. Let cool & bottle."
+, link: "https://www.youtube.com/watch?v=a_I1DNJFvoE&t=12s"},
+{name:  "Building blocks of NI Curries"
+, recipe: "Dairy ( milk, cream, yogurt), Dal, Nut paste, Onion paste, tomato paste, flavoured with garam masala.  Dal: 1C tuvar dal, 2C water, 1tsp ghee. 8 whistles/ 7 mins.  Onion/ Tomato paste : 1tbsp water, 1tbsp oil, 3C (400g)chopped onions/ deseeded tomato, 1tsp sugar, 1/2tsp salt. 6 minutes/ 8 whistles. Mix & match with spiced vegetables. "
+, link: "https://www.youtube.com/watch?v=wOR7rBzsgJk&t=24s"},
+{name:  "Building blocks of SI Curries"
+, recipe: "Dal, coconut paste, Yogurt, Tamarind Dal: 1C tuvar dal, 2C water, 1tsp ghee. 8 whistles/ 7 mins.  Coconut paste: Blend 1/2C coconut, 1tsp cumin, 1 green chilli & 1/2tsp salt.  Tamarind: Cover deseeded tamarind with water. Cook for 4 whistles. Mash & filter.  Mix & match with spiced vegetables."
+, link: "https://www.youtube.com/watch?v=zsp35-yd9zA&t=96s"},
+];
